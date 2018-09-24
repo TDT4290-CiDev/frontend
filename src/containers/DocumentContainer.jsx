3fe2d932f5a7be0fca8c1dc5a83a034c30f6363b
Eivind Reime <22083159+eivrei@users.noreply.github.com>
2018-09-24 13:14:56 +0200

    Add basic layout for form designer

---
+++ src/containers/DocumentContainer.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const DocumentContainer = () => <div className="documentContainer">DocumentContainer</div>;
+
+export default DocumentContainer;
---
+++ src/containers/FormDesignerContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DocumentContainer from './DocumentContainer';
+
+const FormDesignerContainer = () => (
+  <div className="container">
+    <DocumentContainer />
+  </div>
+);
+
+export default FormDesignerContainer;
