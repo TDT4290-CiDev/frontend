e0b0c46f968c17a7b3d941c1ecc61f85277d99b0
Eivind Reime <22083159+eivrei@users.noreply.github.com>
2018-09-24 10:19:00 +0200

    Fix babel/webpack error

--- nwb.config.js
+++ nwb.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   type: 'react-app',
+  webpack: {
+    extra: {
+      entry: `${__dirname}/src/index.jsx`,
+      resolve: {
+        extensions: ['.jsx', '.js'],
+      },
+      module: {
+        rules: [
+          {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react'],
+            },
+          },
+        ],
+      },
+    },
+  },
 };
