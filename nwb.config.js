a8743f30637163eacbf49b39aa0cfb7605efc58a
Eivind Reime <22083159+eivrei@users.noreply.github.com>
2018-09-24 14:24:59 +0200

    Remove eslint rule for using .jsx

--- nwb.config.js
+++ nwb.config.js
@@ -1,23 +1,3 @@
 module.exports = {
   type: 'react-app',
-  webpack: {
-    extra: {
-      entry: `${__dirname}/src/index.jsx`,
-      resolve: {
-        extensions: ['.jsx', '.js'],
-      },
-      module: {
-        rules: [
-          {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-react'],
-            },
-          },
-        ],
-      },
-    },
-  },
 };
