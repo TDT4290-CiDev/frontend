29f303592e6612787338d4cafd3633168d7393e2
Eivind Lie Andreassen <ela4991@gmail.com>
2018-10-21 17:37:42 +0200

    Add support for arrays in macro props

--- src/utils/modules.js
+++ src/utils/modules.js
@@ -6,7 +6,7 @@ import BulletPointListContainer from '../containers/BulletPointListContainer';
 
 // 2. Create a shortcut to the container
 const availableModules = {
-  '[] {option}': TestComponent,
+  '[] {options[]} - {options[]}': TestComponent,
   _: TestComponent,
   __: TestComponent,
   '* {item}': BulletPointListContainer,
@@ -14,7 +14,7 @@ const availableModules = {
 
 // 3. Add a description to the container
 const commandTranslation = {
-  '[] {option}': 'flervalgsspørsmål',
+  '[] {option[]}': 'flervalgsspørsmål',
   _: 'textsvar',
   __: 'langsvar',
   '* {item}': 'punktliste',
@@ -31,7 +31,7 @@ const escapeRegExp = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 
 const getModuleShortcutMatch = moduleShortcut => {
   for(const key of Object.keys(availableModules)) {
-    const patternElements = key.split(/{[a-zA-Z_-]+}/);
+    const patternElements = key.split(/{[a-zA-Z_\[\]\-]+}/);
     const pattern = new RegExp(`^${  patternElements.map(x => x.trim()).map(escapeRegExp).join('.*')  }$`);
     if(moduleShortcut.match(pattern)) {
       return key;
@@ -48,13 +48,22 @@ const getModule = moduleShortcut => {
 
 const getProps = moduleShortcut => {
   const key = getModuleShortcutMatch(moduleShortcut);
-  const propNames = key.match(/{[a-zA-Z_-]+}/g).map(x => x.substring(1, x.length - 1));
-  const delimiters = key.split(/{[a-zA-Z_-]+}/).filter(x=>x);
+  const propNames = key.match(/{[a-zA-Z_\[\]\-]+}/g).map(x => x.substring(1, x.length - 1));
+  const delimiters = key.split(/{[a-zA-Z_\[\]\-]+}/).filter(x=>x);
   const propValues = moduleShortcut.split(new RegExp(delimiters.map(escapeRegExp).join('|'))).filter(x=>x);
 
   const props = Object();
   for(let i = 0; i < propNames.length; i+=1) {
-    props[propNames[i]] = propValues[i];
+    if(propNames[i].endsWith("[]")) {
+      const propName = propNames[i].substring(0, propNames[i].length - 2);
+      if(props[propName]) {
+        props[propName].push(propValues[i]);
+      } else {
+        props[propName] = [propValues[i]];
+      }
+    } else {
+      props[propNames[i]] = propValues[i];
+    }
   }
 
   return props;
