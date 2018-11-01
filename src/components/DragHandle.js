ffe816fb90f6c5bcd029206b66190561f4bea531
afriestad <afriestad@users.noreply.github.com>
2018-11-01 12:20:11 +0100

    Add bullet point and form style for <1920px screens

--- src/components/DragHandle.js
+++ src/components/DragHandle.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SortableHandle } from 'react-sortable-hoc';
 
-const DragHandle = SortableHandle(() => <span className="drag-handle">::</span>);
+const DragHandle = SortableHandle(() => <span className="drag-handle material-icons">swap_vert</span>);
 
 export default DragHandle;
