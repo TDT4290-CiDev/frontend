e9df77bdb8855805f3cbae27ac12503e97136333
Kristina Berg <kristinahberg95@gmail.com>
2018-11-11 14:05:43 +0100

    Add buttons for shortcuts

---
+++ src/components/ShortcutButtons.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ShortcutButtons = ({ onClick }) => (
+  <div className="shortcut-buttons">
+    <button type="button" onClick={() => onClick('_')}>
+      Kortsvar
+    </button>
+    <button type="button" onClick={() => onClick('()')}>
+      Radiobutton
+    </button>
+    <button type="button" onClick={() => onClick('*')}>
+      Punktliste
+    </button>
+  </div>
+);
+
+ShortcutButtons.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default ShortcutButtons;
