b1ccb3e92f3f724273e16d598280907b7900b468
Kristina Berg <kristinahberg95@gmail.com>
2018-11-12 15:51:14 +0100

    Add expandable button to show all shortcut buttons

--- src/components/ShortcutButtons.js
+++ src/components/ShortcutButtons.js
@@ -1,19 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ShortcutButtons = ({ onClick }) => (
-  <div className="shortcut-buttons">
-    <button type="button" onClick={() => onClick('_')}>
-      Kortsvar
-    </button>
-    <button type="button" onClick={() => onClick('()')}>
-      Radiobutton
-    </button>
-    <button type="button" onClick={() => onClick('[]')}>
-      Checkbox
-    </button>
-  </div>
-);
+class ShortcutButtons extends React.Component {
+  state = { check: false };
+
+  render() {
+    const { check } = this.state;
+    const { onClick } = this.props;
+    return (
+      <div className="shortcut-buttons">
+        <button type="button" onClick={() => onClick('_')}>
+          Kortsvar
+        </button>
+        <button type="button" onClick={() => onClick('()')}>
+          Radiobutton
+        </button>
+        <button type="button" onClick={() => onClick('[]')}>
+          Checkbox
+        </button>
+        <button
+          type="button"
+          onClick={() =>
+            this.setState(prevState => ({
+              check: !prevState.check,
+            }))
+          }
+        >
+          Flere knapper
+        </button>
+        <div className="expandable-shortcut-buttons">
+          {check && (
+            <div>
+              <button type="button" onClick={() => onClick('__')}>
+                Langsvar
+              </button>
+              <button type="button" onClick={() => onClick('*')}>
+                Punktliste
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
 
 ShortcutButtons.propTypes = {
   onClick: PropTypes.func.isRequired,
