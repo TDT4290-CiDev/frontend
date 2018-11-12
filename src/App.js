3dd3b6c9880166cf42f0d6b4b62aab11befc9b93
Eivind Reime <22083159+eivrei@users.noreply.github.com>
2018-11-12 15:10:02 +0100

    Add ability to preview forms

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { hot } from 'react-hot-loader';
 import './styles/base.scss';
 import FormDesignerContainer from './containers/FormDesignerContainer';
+import FormPreviewContainer from './containers/FormPreviewContainer';
 import FormLandingPage from './containers/FormLandingPage';
 import FormListContainer from './containers/FormListContainer';
 
@@ -12,6 +13,7 @@ const App = () => (
       <Route exact path="/" component={FormLandingPage} />
       <Route path="/new-form" component={FormDesignerContainer} />
       <Route path="/edit-form/:id" component={FormDesignerContainer} />
+      <Route path="/preview-form/:id" component={FormPreviewContainer} />
       <Route path="/existing-forms" component={FormListContainer} />
     </div>
   </Router>
