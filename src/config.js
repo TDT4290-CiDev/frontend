cee3be55f439fbec4759f84e8eb0c09c144f17b9
Eivind Reime <22083159+eivrei@users.noreply.github.com>
2018-11-14 12:33:47 +0100

    Fix #20. Use root url as base url for api

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-undef */
-const API_URL = NODE_ENV === 'production' ? 'https://cidev.idi.ntnu.no/api/' : 'http://localhost:8080/api/';
+const API_URL = NODE_ENV === 'production' ? `${window.location.origin}/api/` : 'http://localhost:8080/api/';
 
 export default API_URL;
